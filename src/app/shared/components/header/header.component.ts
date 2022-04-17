c1c7e471b9f2f38d95d9dc8bba16990e467234bd
OlhaPelekh <olhapelekh@hotmail.com>
2022-04-17 23:44:34 +0300

    3

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,  OnInit } from '@angular/core';
+// import * as $ from 'jquery';
+ declare var $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    $('.dropdown').dropdown();
 
+    $('.ui.sidebar').first().sidebar('attach events', '.ui.icon.item .bars');
+  }
 }
